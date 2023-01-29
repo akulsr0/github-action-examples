8ba1289e4b0ee665d677659907e0fb3a124fd349
Akul Srivastava <akulsr0@gmail.com>
2023-01-29 20:08:18 +0530

    chore: deployment preview for pull requests (#12)
    * chore: deployment preview
    
    * get branch name
    
    * fix
    
    * check
    
    * check1
    
    * fix1
    
    * check2
    
    * check 3
    
    * chekc 4
    
    * check 6
    
    * check 7
    
    * check 8
    
    * add preview comment
    
    * check
    
    * checkk
    
    * sticky comment
    
    * check
    
    * check
    
    * update
    
    * done
    
    * final

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 export default function App() {
   return (
     <main>
-      <h1>Github Actions Examples</h1>
+      <h1>Github Action Examples</h1>
+      <a
+        href="https://github.com/akulsr0/github-action-examples"
+        target="_blank"
+      >
+        [github]
+      </a>
     </main>
   );
 }
