7cd3f1435353dcf1b27d2be027cd67ae9c9ae367
Akul Srivastava <akulsr0@gmail.com>
2023-01-29 17:59:59 +0530

    chore: deployment preview

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ export default function App() {
   return (
     <main>
       <h1>Github Actions Examples</h1>
+      <a
+        href="https://github.com/akulsr0/github-action-examples"
+        target="_blank"
+      >
+        [github]
+      </a>
     </main>
   );
 }
