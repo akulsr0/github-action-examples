61278ec0f20aa1b16cb25db96537500270ccaa7a
Akul Srivastava <akulsr0@gmail.com>
2023-02-04 13:44:34 +0530

    chore: demo update to test preview workflow

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ export default function App() {
   return (
     <main>
       <h1>Github Action Examples</h1>
+      <p>Hello World!</p>
       <a
         href="https://github.com/akulsr0/github-action-examples"
         target="_blank"
