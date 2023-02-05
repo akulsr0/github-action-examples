493f91578b7ba2f37ea727a9c39137340c47a42c
Akul Srivastava <akulsr0@gmail.com>
2023-02-05 20:16:51 +0530

    check

---
+++ build-size.js
@@ -0,0 +1,22 @@
+import getFolderSize from 'get-folder-size';
+
+function humanFileSize(size) {
+  var i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
+  return (
+    (size / Math.pow(1024, i)).toFixed(2) * 1 +
+    ' ' +
+    ['B', 'kB', 'MB', 'GB', 'TB'][i]
+  );
+}
+
+export const getSize = async () => {
+  const size = await getFolderSize.loose('./dist');
+  const readableSize = humanFileSize(size);
+  console.log('---> 1', size, readableSize);
+  return {
+    size: {
+      bytes: size,
+      readableString: readableSize,
+    },
+  };
+};
