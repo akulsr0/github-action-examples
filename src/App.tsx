e3de236273e9ead8e0d3625fa37ae804690cf76c
akul-smallcase <116796624+akul-smallcase@users.noreply.github.com>
2023-07-18 11:42:55 +0530

    chore: try build compare github action

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import image from './assets/image.png';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       >
         [github]
       </a>
+      <img src={image} alt="my-image" />
     </main>
   );
 }
