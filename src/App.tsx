ce8b4a6eb538db061e998932a3cc81d0112b8e36
akul-smallcase <116796624+akul-smallcase@users.noreply.github.com>
2023-07-27 17:12:44 +0530

    chore: test build comparison

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import githubImg from './assets/github.png';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       >
         [github]
       </a>
+      <img src={githubImg} alt="meow" />
     </main>
   );
 }
